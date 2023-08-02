fe8e1fca9962c520cb33e71787e87e32e1be7584
Alexis Bernal <alexis.ferber18@gmail.com>
2023-08-02 17:48:32 -0600

    Includes Props and stylo for the title
    The ShowName Prop has been implemented to display the component with or without a name depending on what is needed

---
+++ renta_facil_colima/Components/Logo/LogoRFC.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+
+const LogoRFC = ({ showAppName}) => {
+    return (
+    <View>
+    <Image
+        source={require('../../Components/Logo/LogoRFC.png')}
+        style={{width: 200, height: 200}} 
+    />
+    {showAppName && <Text style={styles.AppName}>Renta Facil</Text>}
+    </View>
+);
+};
+
+export default LogoRFC;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+    },
+    AppName: {
+        textAlign: 'center',
+    },
+});
