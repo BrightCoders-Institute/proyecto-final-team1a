8091421dae6b0fa1d6bcf5c17218f5f0af0afdc3
BrandonVG <brandon.vadillox3@gmail.com>
2023-08-08 22:18:18 -0600

    Created navigation with stack and bottom tabs
    App.jsx
      - Added navigation to app
      - Added paper provider to app
      - Created fonts config
      - Created theme for react native paper that uses fonts config
    
    MainActivity.java
      - Modified to complete react native navigation installation
    
    BottomTabsNavigation.jsx
      - Created bottom tabs navigation
      - For now it a temporal component to avoid app crashing
      - It is already configured to future screens
      - For now 2 tabs are commented until the screens are created
      - For now 2 tabs have a temporal component until the screens are created
    
    MainNavigation.jsx
      - Created main navigation with stack navigation
      - Added bottom tabs navigation to main navigation as HomeTabs
      - It is already configured to future screens
      - For now the navigation have various screens commented until the screens are created

--- renta_facil_colima/App.jsx
+++ renta_facil_colima/App.jsx
@@ -1,69 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import BasicButton from './src/View/buttons/BasicButton';
-import GoogleButton from './src/View/buttons/GoogleButton';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigation from './src/navigation/MainNavigation';
+import {
+  configureFonts,
+  Provider as PaperProvider,
+  DefaultTheme,
+} from 'react-native-paper';
+
+const fontsConfig = {
+  android: {
+    regular: {
+      fontFamily: 'ConcertOne-Regular',
+      fontWeight: 'normal',
+    },
+  },
+};
+
+const theme = {
+  ...DefaultTheme,
+  fonts: configureFonts(fontsConfig),
+};
 
 function App() {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#FFF', alignItems:'center'}}>
-      <BasicButton
-        text="Login"
-        onPress={() => console.log('Hello World')}
-        textSize={20}
-        borderColor="#058C42"
-        backgroundColor="#FFF"
-        height={59}
-        elevation={10}
-        textColor='#00D25D'
-        width={150}
-        shadow={true}
-      />
-      <BasicButton
-        text="Sign up"
-        textSize={20}
-        onPress={() => console.log('Hello World')}
-        borderColor="#B1F1D1"
-        backgroundColor="#FFF"
-        height={59}
-        elevation={0}
-        textColor='#9E9A9A'
-        width={150}
-        shadow={false}
-      />
-      <BasicButton
-        text="Sign up"
-        textSize={32}
-        onPress={() => console.log('Hello World')}
-        borderColor="#058C42"
-        backgroundColor="#FFF"
-        height={74}
-        elevation={0}
-        textColor='#00D25D'
-        width={308}
-        shadow={true}
-      />
-      <BasicButton
-        text="Cancelar"
-        onPress={() => console.log('Hello World')}
-        textSize={20}
-        borderColor="#058C42"
-        backgroundColor="#04471C"
-        height={59}
-        elevation={10}
-        textColor='#FFF'
-        width={150}
-        shadow={true}
-        shadowColor='#0f9444'
-      />
-      <GoogleButton onPress={() => console.log('Hello World')} />
-    </SafeAreaView>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <MainNavigation />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
