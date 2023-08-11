d7e5c992e1a3b38e5ac287bee24cfe0b8bf04f9c
BrandonVG <brandon.vadillox3@gmail.com>
2023-08-11 17:47:57 -0600

    Comments component fixes
    Added some font fixes like font family and font colors to the component
    also added some spacing fixes

--- renta_facil_colima/src/View/components/CommentComponent.jsx
+++ renta_facil_colima/src/View/components/CommentComponent.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import styles from '../../styles/CommentComponentstyle';
 
-const CommentComponent = ({text, comment}) => {
+const CommentComponent = ({text, comment, date}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}> {text}</Text>
-      <Text style={styles.comment}> {comment} </Text>
+      <Text style={styles.titulo}>{text} - {date}</Text>
+      <Text style={styles.comment}>{comment} </Text>
     </View>
   );
 };
--- renta_facil_colima/src/styles/CommentComponentstyle.js
+++ renta_facil_colima/src/styles/CommentComponentstyle.js
@@ -1,34 +1,33 @@
 import {StyleSheet} from 'react-native';
 const CommentComponentStyles = StyleSheet.create({
   container: {
-    padding: 20,
+    alignSelf: 'center',
     backgroundColor: '#f5f5f5',
     borderWidth: 1,
     borderColor: '#058C42',
     backgroundColor: '#fff',
     borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    marginVertical: 10,
-    marginHorizontal: 10,
     shadowColor: 'black',
     shadowOpacity: 0.3,
     shadowOffset: {width: 0, height: 2},
     shadowRadius: 5,
     elevation: 5,
+    width: '95%',
   },
   titulo: {
     fontSize: 28,
-    fontWeight: 'bold',
-    fontFamily: 'ConcertOne',
-    textAlign: 'center',
-    textAlign: 'left',
+    paddingTop: 3,
+    marginLeft: 8,
+    color: '#000',
+    fontFamily: 'ConcertOne-Regular',
+
   },
   comment: {
+    marginLeft: 8,
+    paddingBottom: 10,
     fontSize: 24,
-    textAlign: 'center',
+    fontFamily: 'ConcertOne-Regular',
     color: 'gray',
-    textAlign: 'left',
   },
 });
 
