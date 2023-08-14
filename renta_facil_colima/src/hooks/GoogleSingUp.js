2e85dc02d31d0211f5fc4086a2f05532ee82ab7d
axel arredondo <axelomar.a.v@gmail.com>
2023-08-14 17:18:47 -0600

    Fixed login method with google account

---
+++ renta_facil_colima/src/hooks/GoogleSingUp.js
@@ -0,0 +1,25 @@
+import {initializeApp} from 'firebase/app';
+import {firebaseConfig} from '../Firebase/FirebaseConfig';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { getAuth, signInWithCredential } from "firebase/auth";
+import Config from 'react-native-config';
+initializeApp(firebaseConfig);
+
+GoogleSignin.configure({
+  webClientId: Config.WEB_CLIENT_ID,
+});
+
+  const GoogleSignUp = async () => {
+    try {
+     await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      const googleCredential = GoogleAuthProvider.credential(userInfo.idToken);
+      const auth = getAuth();
+      const userCredential = await signInWithCredential(auth, googleCredential);
+      const user = userCredential.user;
+      console.log('User:', user);
+    } catch (error) {
+      console.error('Google Sign-In error:', error);
+    }
+  };
+  export default GoogleSignUp;
