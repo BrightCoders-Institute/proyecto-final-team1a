20ce4e743e98fc30a2c13355a237d59178f28303
axel arredondo <axelomar.a.v@gmail.com>
2023-08-24 23:55:07 -0600

    Add function to get img and Carrucel component.
    Add the function to get images by id and a collection and create a photo
    carousel, which renders the images of the properties that belong to that
    id.

---
+++ renta_facil_colima/src/View/components/Carrucel.jsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react'
+import GetImages from '../../hooks/GetImages'
+import { FlatList, Image } from 'react-native'
+
+export const Carrucel = () => {
+    const [imageUrls, setImageUrls] = useState([]);
+    
+    useEffect (() => {
+        const fetchData = async () => {
+            //En un futuro se cambiara el casa1 por el id de la propiedad  actual 
+            const urls = await GetImages('casa1');
+            setImageUrls(urls);
+        };  
+
+       fetchData();
+    }, []);
+  return (
+    <FlatList
+        data={imageUrls}
+        pagingEnabled={true}
+        horizontal={true}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item})=> (
+        <Image source ={{uri: item}} style={{width:400,height:300,padding:5,margin:5}}/> 
+        )}
+    />
+  )
+}
