f69443bf18de4aa6b27e08357d7888cb56032297
Brandon Vadillo <brandon.vadillox3@gmail.com>
2023-08-25 19:08:59 -0600

    Add houses form and other improves
    App.jsx
      - Add react navigation safe area provider
    
    formAdd.jsx
      - Recoded the form
      - Add form validation with formik
    
    formSendHouse.js
      - Add form validation with yup
      - Add state of the modal
      - function to handle the add house
    
    useAddHouse.js
      - Added the id of the current user at creating a house
    
    Colors.js
      - Add new colors
    
    ReusableMainScreen.jsx
      - Added the add house form
      - Added FAB button to open the add house form
      - Used safe area of navigation
    
    UtilsStyle.js
      - Add new styles
    
    formAddStyle.js
      - Recoded the styles

--- renta_facil_colima/src/hooks/useAddHouse.js
+++ renta_facil_colima/src/hooks/useAddHouse.js
@@ -1,32 +1,32 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { firebaseConfig } from "../Firebase/FirebaseConfig";
-
-
+import {initializeApp} from 'firebase/app';
+import {getFirestore, collection, addDoc} from 'firebase/firestore';
+import {firebaseConfig} from '../Firebase/FirebaseConfig';
+import GetCurrentUser from '../hooks/GetCurrentUser';
+import {Alert} from 'react-native';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-const AddHouse = async (title, address, rooms,bathrooms, surface, rent ) => {
+const AddHouse = async (title, address, rooms, bathrooms, surface, rent) => {
+  try {
+    const user = GetCurrentUser();
+    const docRef = await addDoc(collection(db, 'Houses'), {
+      title: title,
+      address: address,
+      rooms: rooms,
+      bathrooms: bathrooms,
+      surface: surface,
+      rent: rent,
+      userId: user.uid,
+    });
 
-try {
-  const docRef = await addDoc(collection(db, "Houses"), {
-    title: title,
-    address: address,
-    rooms: rooms,
-    bathrooms: bathrooms, 
-    surface: surface,
-    rent: rent,
-  });
-
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+    return docRef.id;
+  } catch (e) {
+    Alert.alert('Error', e.message);
+  }
 };
 
 export default AddHouse;
--- renta_facil_colima/src/styles/UtilsStyle.js
+++ renta_facil_colima/src/styles/UtilsStyle.js
@@ -8,6 +8,21 @@ const UtilsStyle = StyleSheet.create({
     fontFamily: 'ConcertOne-Regular',
     fontSize: 16,
   },
+  row: {
+    flexDirection: 'row',
+  },
+  rowCentered: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  rowSpaceAround: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  rowSpaceBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
 
 export default UtilsStyle;
