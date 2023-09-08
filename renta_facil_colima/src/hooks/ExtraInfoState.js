912b61d845b69356d88e030b6b3ba43a1ff39def
Alexis Bernal <alexis.ferber18@gmail.com>
2023-09-07 18:12:21 -0600

    Fixed DateSelector
    Fixed

--- renta_facil_colima/src/hooks/ExtraInfoState.js
+++ renta_facil_colima/src/hooks/ExtraInfoState.js
@@ -1,12 +1,17 @@
 import * as Yup from 'yup';
 
+const fechaMinima = Date.now() - 568024668000
+
 const createExtraInfoSchema = () => {
   return Yup.object().shape({
     address: Yup.string().required('Address field can not be empty'),
     profilePicture: Yup.string().required(
       'Profile Picture field can not be empty',
     ),
-    birthday: Yup.date().required('Birthday field can not be empty'),
+    birthday: Yup.date().required('Birthday field can not be empty').max(new Date( fechaMinima),'The minimum age is 18 years' ),
+
+
+
   });
 };
 
@@ -18,7 +23,7 @@ const useExtraInfoState = () => {
   };
 
   const extraInfoSchema = createExtraInfoSchema();
-
+  console.log((new Date((fechaMinima) )))
   return {
     extraInfoSchemaFormInitialValues,
     extraInfoSchema,
